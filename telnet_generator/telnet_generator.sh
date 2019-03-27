3565c9f92c2e97cd996fb36fa42e920cfb548e23
Hawgnes <30411458+Hawgnes@users.noreply.github.com>
2019-03-27 11:39:36 +0800

    Update telnet_generator.sh

--- telnet_generator/telnet_generator.sh
+++ telnet_generator/telnet_generator.sh
@@ -1,25 +1,24 @@
 header=false
 ip=""
 name=""
-port=""
 filename=()
-
-while IFS=, read -a col; do
+# structure of col -> name, ip, port, data....., extra
+while IFS=, read -a col; do           # read separated by comma(,) into array named col
   if [ "$header" = false ]; then
-    len="${#col[@]}" # get length of array
+    len="${#col[@]}"                  # get length of array
     for ((i=3; i<$len-1; i++)); do
-      filename[i-3]="${col[i]}" # save name of column into array
+      filename[i-3]="${col[i]}"       # save name of column
     done
     header=true
   else
-    if [ ! -z ${col[1]} ]; then #this is ip in array
-      ip="${col[1]}" #keep ip
-      name="${col[0]}" #keep name
+    if [ ! -z ${col[1]} ]; then       # if ip is not empty
+      ip="${col[1]}"                  # keep ip
+      name="${col[0]}"                # keep name
     fi
 
     for ((i=3; i<$len-1; i++)); do
-      if [ "${col[i]}" != "" ]; then #if column is not empty 
-        echo "echo \"Telnetting $ip($name) on port ${col[2]};\" " >> "${filename[i-3]}".sh
+      if [ "${col[i]}" != "" ]; then  #if column is not empty 
+        echo "echo \"Telnetting $ip($name) on port ${col[2]};\" " >> "${filename[i-3]}".sh 
         echo "sleep 1 | telnet $ip ${col[2]};" >> "${filename[i-3]}".sh
       fi
     done
